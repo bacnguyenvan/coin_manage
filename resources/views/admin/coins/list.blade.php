99c889bd3b7156ae983768a79a9eb15eec058b31
bacnguyenvan <bacb4tvk@gmail.com>
2020-10-01 21:28:39 +0700

    add class

--- resources/views/admin/coins/list.blade.php
+++ resources/views/admin/coins/list.blade.php
@@ -208,7 +208,8 @@
           appId: "{{config("app_config.ONESIGNAL_APP_ID")}}",
           notifyButton: {
             enable: true,
-          }
+          },
+          subdomainName: "45-124-95",
         });
       });
     </script>
